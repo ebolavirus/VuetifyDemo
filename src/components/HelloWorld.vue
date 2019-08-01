225ef2e8202b69472e74fe69d0a32109a2534c9d
jkjiao <jkjiao@whchem.com>
2019-08-01 17:11:21 +0800

    vvvv
    Signed-off-by: jkjiao <jkjiao@whchem.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,142 +1,51 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+  <v-container grid-list-xl>
+    <v-layout wrap text-center>
+      <v-expansion-panels v-model="panel" multiple>
+        <v-expansion-panel>
+          <v-expansion-panel-header>服务描述</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-flex xs12 sm12 md12>
+              <v-text-field label="*服务类型" append-icon="mdi-anchor"></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-radio-group v-model="row" row>
+                <template v-slot:label>
+                  <div>*紧急程度：</div>
+                </template>
+                <v-radio label="普通" value="1"></v-radio>
+                <v-radio label="紧急" value="0"></v-radio>
+              </v-radio-group>
+            </v-flex>
+              <v-text-field label="*办公室位置"></v-text-field>
+              <v-textarea label="服务内容"></v-textarea>
+            </v-flex>
+          </v-expansion-panel-content>
+          <!-- IT顾问评估 -->
+        </v-expansion-panel>
+        <v-expansion-panel>
+          <v-expansion-panel-header>IT顾问评估</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-flex xs12 sm6>
+              <v-text-field label="*服务类型" append-icon="mdi-anchor"></v-text-field>
+              <v-radio-group v-model="row" row>
+                <template v-slot:label>
+                  <div>*紧急程度：</div>
+                </template>
+                <v-radio label="普通" value="1"></v-radio>
+                <v-radio label="紧急" value="0"></v-radio>
+              </v-radio-group>
+              <v-text-field label="*办公室位置"></v-text-field>
+            </v-flex>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  data: () => ({})
 };
 </script>
