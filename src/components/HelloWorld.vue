85247e124f5d553adeb064227977390a24b6fc44
jkjiao <jkjiao@whchem.com>
2019-08-02 17:47:49 +0800

    ccc
    Signed-off-by: jkjiao <jkjiao@whchem.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,25 +11,24 @@
           </v-expansion-panel-header>
           <v-expansion-panel-content>
             <v-layout wrap>
-            <v-flex xs4>
-              <v-text-field label="*服务类型" append-icon="mdi-anchor"></v-text-field>
-            </v-flex>
-            <v-flex xs4>
-              <v-radio-group v-model="row" row>
-                <template v-slot:label>
-                  <div>*紧急程度：</div>
-                </template>
-                <v-radio label="普通" value="1"></v-radio>
-                <v-radio label="紧急" value="0"></v-radio>
-              </v-radio-group>
-            </v-flex>
-            <v-flex xs4>
-              <v-text-field label="*办公室位置"></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <header>服务内容</header>
-              <v-text-field label="*办公室位置"></v-text-field>
-            </v-flex>
+              <v-flex xs6>
+                <v-text-field label="*服务类型" append-icon="mdi-anchor"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-radio-group v-model="row" row>
+                  <template v-slot:label>
+                    <div>*紧急程度：</div>
+                  </template>
+                  <v-radio label="普通" value="1"></v-radio>
+                  <v-radio label="紧急" value="0"></v-radio>
+                </v-radio-group>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="*办公室位置"></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea label="服务内容"></v-textarea>
+              </v-flex>
             </v-layout>
           </v-expansion-panel-content>
           <!-- IT顾问评估 -->
@@ -37,41 +36,98 @@
         <v-expansion-panel>
           <v-expansion-panel-header>IT顾问评估</v-expansion-panel-header>
           <v-expansion-panel-content>
-            <v-flex xs12 sm12>
-              <v-textarea label="评估意见"></v-textarea>
-              <v-radio-group v-model="row" row xs6 sm3 md3>
-                <template v-slot:label>
-                  <div>变更等级：</div>
-                </template>
-                <v-radio label="较小" value="0"></v-radio>
-                <v-radio label="中等" value="1"></v-radio>
-                <v-radio label="较大" value="2"></v-radio>
-              </v-radio-group>
-              <v-select :items="serviceTypes" label="服务分类" xs6 sm3 md3></v-select>
-              <v-text-field label="预估费用"></v-text-field>
-              <v-text-field label="预估实施"></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm12 md12>
-              <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm12 md12>
-              <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
-            </v-flex>
+            <v-layout wrap>
+              <v-flex xs12>
+                <v-textarea label="评估意见"></v-textarea>
+                <v-radio-group v-model="row" row xs6 sm3 md3>
+                  <template v-slot:label>
+                    <div>变更等级：</div>
+                  </template>
+                  <v-radio label="较小" value="0"></v-radio>
+                  <v-radio label="中等" value="1"></v-radio>
+                  <v-radio label="较大" value="2"></v-radio>
+                </v-radio-group>
+              </v-flex>
+              <v-flex xs4>
+                <v-select :items="serviceTypes" label="服务分类" xs6 sm3 md3></v-select>
+              </v-flex>
+              <v-flex xs4>
+                <v-text-field label="预估费用"></v-text-field>
+              </v-flex>
+              <v-flex xs4>
+                <v-text-field label="预估实施"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
+              </v-flex>
+            </v-layout>
           </v-expansion-panel-content>
         </v-expansion-panel>
         <!-- 办理人办理 -->
         <v-expansion-panel>
           <v-expansion-panel-header>办理人办理</v-expansion-panel-header>
           <v-expansion-panel-content>
-            <v-flex xs12 sm12 md12>
-              <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
-            </v-flex>
-            <v-flex xs12 sm12 md12>
-              <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
-            </v-flex>
+            <v-layout wrap>
+              <v-flex xs6>
+                <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field label="实际费用"></v-text-field>
+              </v-flex>
+              <v-flex xs6>
+                <v-text-field label="实际实施"></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea label="办理过程"></v-textarea>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea label="原因分析"></v-textarea>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea label="结果及建议"></v-textarea>
+              </v-flex>
+            </v-layout>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <!--  -->
+        <v-expansion-panel>
+          <v-expansion-panel-header>申请人验收</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-layout wrap>
+              <v-flex xs6>
+                <v-radio-group row>
+                  <template v-slot:label>
+                    <div>是否解决：</div>
+                  </template>
+                  <v-radio label="已解决" value="0"></v-radio>
+                  <v-radio label="未解决" value="1"></v-radio>
+                </v-radio-group>
+              </v-flex>
+              <v-flex xs6>
+                <v-radio-group row>
+                  <template v-slot:label>
+                    <div>是否满意：</div>
+                  </template>
+                  <v-radio label="满意" value="0"></v-radio>
+                  <v-radio label="不满意" value="1"></v-radio>
+                </v-radio-group>
+              </v-flex>
+              <v-flex xs12>
+                <v-textarea label="验收报告及建议"></v-textarea>
+              </v-flex>                            
+            </v-layout>
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
+      <iframe>
+        
+      </iframe>
     </v-layout>
   </v-container>
 </template>
