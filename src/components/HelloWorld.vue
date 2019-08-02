5577f60a578821b8f9fb90a32040aa3488a12212
jkjiao <jkjiao@whchem.com>
2019-08-02 10:16:41 +0800

    nnn Signed-off-by: jkjiao <jkjiao@whchem.com>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-xl>
     <v-layout wrap text-center>
-      <v-expansion-panels v-model="panel" multiple >
+      <v-expansion-panels v-model="panel" multiple>
         <v-expansion-panel>
           <v-expansion-panel-header>服务描述</v-expansion-panel-header>
           <v-expansion-panel-content>
@@ -27,16 +27,51 @@
         <v-expansion-panel>
           <v-expansion-panel-header>IT顾问评估</v-expansion-panel-header>
           <v-expansion-panel-content>
-            <v-flex xs12 sm6>
-              <v-text-field label="*服务类型" append-icon="mdi-anchor"></v-text-field>
-              <v-radio-group v-model="row" row>
+            <v-flex xs12 sm12>
+              <v-textarea label="评估意见"></v-textarea>
+              <v-radio-group v-model="row" row xs6 sm3 md3>
                 <template v-slot:label>
-                  <div>*紧急程度：</div>
+                  <div>变更等级：</div>
                 </template>
-                <v-radio label="普通" value="1"></v-radio>
-                <v-radio label="紧急" value="0"></v-radio>
+                <v-radio label="较小" value="0"></v-radio>
+                <v-radio label="中等" value="1"></v-radio>
+                <v-radio label="较大" value="2"></v-radio>
               </v-radio-group>
-              <v-text-field label="*办公室位置"></v-text-field>
+              <v-select :items="serviceTypes" label="服务分类" xs6 sm3 md3></v-select>
+              <v-text-field label="预估费用"></v-text-field>
+              <v-text-field label="预估实施"></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
+            </v-flex>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+        <!-- 办理人办理 -->
+        <v-expansion-panel>
+          <v-expansion-panel-header>办理人办理</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-flex xs12 sm12>
+              <v-textarea label="评估意见"></v-textarea>
+              <v-radio-group v-model="row" row xs6 sm3 md3>
+                <template v-slot:label>
+                  <div>变更等级：</div>
+                </template>
+                <v-radio label="较小" value="0"></v-radio>
+                <v-radio label="中等" value="1"></v-radio>
+                <v-radio label="较大" value="2"></v-radio>
+              </v-radio-group>
+              <v-select :items="serviceTypes" label="服务分类" xs6 sm3 md3></v-select>
+              <v-text-field label="预估费用"></v-text-field>
+              <v-text-field label="预估实施"></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-text-field label="办理人员名称" append-icon="mdi-anchor"></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm12 md12>
+              <v-text-field label="通知人员" append-icon="mdi-anchor"></v-text-field>
             </v-flex>
           </v-expansion-panel-content>
         </v-expansion-panel>
@@ -48,7 +83,8 @@
 <script>
 export default {
   data: () => ({
-    panel: [0,1,2,3,4]
+    panel: [0, 1, 2, 3, 4],
+    serviceTypes: ["type1", "type2", "type3", "type4", "type5"]
   })
 };
 </script>
