b09198b045dbde693f78d06558a506691e9f202d
MingmingSun <xdog2@163.com>
2019-08-06 16:03:40 +0800

    demo logic for type choose is finished.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,32 +14,7 @@
               <v-flex xs4>
                 <v-text-field label="*服务类型" append-outer-icon="mdi-file-document-box-search" readonly
                   @click:append-outer.stop="typeSelect" v-model="typeValue"></v-text-field>
-                <v-dialog v-model="typedialog" persistent max-width="900px">
-                  <v-card>
-                    <v-card-title>
-                      <span>变更类型选择列表</span>
-                    </v-card-title>
-                    <v-card-text>
-                      <small>*特别提醒：正确地选择变更类型将大大加快流程处理速度，如果不清楚该选择何种变更，请致电0535-3388800。</small>
-                      <v-container grid-list-md>
-                        <v-layout wrap>
-                          <v-flex xs12>
-                            <v-data-table :headers="typeheaders" :items="typedesserts" :items-per-page="5"
-                              class="elevation-1">
-                              <template v-slot:item.action="{ item }">
-                                <v-btn class="mb-2" @click="editItem(item)">选择</v-btn>
-                              </template>
-                            </v-data-table>
-                          </v-flex>
-                        </v-layout>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn color="blue darken-1" text @click="typedialog = false">关闭</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
+                <TypeDialog v-model="typedialog" @dialogSelected="typeDialogSelected" />
               </v-flex>
               <v-flex xs4>
                 <v-radio-group v-model="row" row>
@@ -173,117 +148,16 @@
 </template>
 
 <script>
+  import TypeDialog from './TypeDialog';
   export default {
+    components: {
+      TypeDialog
+    },
     data: () => ({
       panel: [0, 1, 2, 3, 4, 5, 6],
       serviceTypes: ["type1", "type2", "type3", "type4", "type5"],
       typeValue: '',
-      typedialog: false,
-      typeheaders: [{
-          text: '变更类型',
-          align: 'left',
-          sortable: false,
-          value: 'name',
-        },
-        {
-          text: '公司范围',
-          value: 'calories'
-        },
-        {
-          text: 'IT业务顾问',
-          value: 'fat'
-        },
-        {
-          text: '说明',
-          value: 'carbs'
-        },
-        {
-          text: '操作',
-          value: 'action',
-          sortable: false
-        }
-      ],
-      typedesserts: [{
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ]
+      typedialog: false
     }),
     methods: {
       typeSelect() {
@@ -291,8 +165,9 @@
         // this.typeValue = aaa;
         this.typedialog = !this.typedialog;
       },
-      editItem(aItem) {
-        console.log('aaaaa', aItem);
+      typeDialogSelected(value) {
+        console.log('bbbbb', value);
+        this.typeValue = value.name;
       }
     }
   };
