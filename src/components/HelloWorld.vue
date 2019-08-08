91012d60bea85d9ddc57a61222eaf9d7351fbe65
MingmingSun <xdog2@163.com>
2019-08-08 15:58:22 +0800

    radio package up.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,13 +17,7 @@
                 <TypeDialog v-model="typedialog" @dialogSelected="typeDialogSelected" />
               </v-flex>
               <v-flex xs4>
-                <v-radio-group v-model="row" row>
-                  <template v-slot:label>
-                    <div>*紧急程度：</div>
-                  </template>
-                  <v-radio label="普通" value="1"></v-radio>
-                  <v-radio label="紧急" value="0"></v-radio>
-                </v-radio-group>
+                <WanhuaRadio />
               </v-flex>
               <v-flex xs4>
                 <v-text-field label="*办公室位置"></v-text-field>
@@ -160,11 +154,13 @@
   import TypeDialog from './TypeDialog';
   import HumanDialog from './HumanDialog';
   import TextfieldReadonly from './TextfieldReadonly';
+  import WanhuaRadio from './WanhuaRadio';
   export default {
     components: {
       TypeDialog,
       HumanDialog,
-      TextfieldReadonly
+      TextfieldReadonly,
+      WanhuaRadio
     },
     data: () => ({
       panel: [0, 1, 2, 3, 4, 5, 6],
---
+++ src/components/WanhuaRadio.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="v-input v-input--is-label-active v-input--is-dirty theme--light v-text-field v-text-field--is-booted">
+    <div class="v-input__control">
+      <div class="v-input__slot">
+        <div class="v-text-field__slot"><label for="input-81" class="v-label v-label--active theme--light"
+            style="left: 0px; right: auto; position: absolute;">*紧急程度</label>
+          <span id="input-81" type="text">
+            <v-radio-group v-model="row" row>
+              <v-radio label="普通" value="1"></v-radio>
+              <v-radio label="紧急" value="0"></v-radio>
+            </v-radio-group>
+          </span>
+        </div>
+      </div>
+      <div class="v-text-field__details">
+        <div class="v-messages theme--light">
+          <div class="v-messages__wrapper"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    data: () => ({
+      row: []
+    }),
+    methods: {}
+  };
+</script>
+<style scoped>
+  .v-text-field>.v-input__control>.v-input__slot:before {
+    border-style: none;
+  }
+</style>
